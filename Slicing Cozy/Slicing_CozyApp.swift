e1136917a0e45c5222d068a69e95ebcc71970289
Magistra <magistraagis@gmail.com>
2023-06-25 13:50:27 +0800

    Initial Commit

---
+++ Slicing Cozy/Slicing_CozyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slicing_CozyApp.swift
+//  Slicing Cozy
+//
+//  Created by magistra aptam on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Slicing_CozyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
